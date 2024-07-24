218277be0067acd9913c66963912b76a773d3127
Marcx <haitianhero6@gmail.com>
2024-07-23 20:39:13 -0400

    StableCoin contract

---
+++ src/StableCoin.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {ERC20Burnable, ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/**
+ * @title StableCoin 
+ * @author Bowtiedgojo
+ * @notice This contract is a stable coin contract that will be used to create a stable coin
+ * Collataial: Exogenous
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * This is the contract meant to be governed by DSC engine. This contract is just the ERC20 
+ * implementation of the stablecoin system. 
+ */
+contract StableCoin  is ERC20Burnable, Ownable{
+
+    error StableCoin__MustBeGreaterThanZero();
+    error StableCoin__BurnAmountExceedsBalance();
+    error StableCoin__NotZeroAddress();
+
+     address owner;
+
+  
+    constructor() ERC20("Stablecoin", "STBL")Ownable(owner){
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <= 0) {
+            revert StableCoin__MustBeGreaterThanZero();
+        }
+        if(balance < _amount) {
+            revert StableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner returns(bool) {
+        if(_to == address(0)) {
+            revert StableCoin__NotZeroAddress();
+        }
+        if(_amount <= 0) {
+            revert StableCoin__MustBeGreaterThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+
+}
