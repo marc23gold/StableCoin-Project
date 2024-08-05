2348507191b18da843463bbc26a1edf8e5cb93b8
Marcx <haitianhero6@gmail.com>
2024-08-05 18:40:11 -0400

    wrapping tokens

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -6,14 +6,14 @@ pragma solidity ^0.8.0;
  * @title DSCEngine
  * @author Bowtiedgojo
  * @notice This is the system that maintains the stability of the stablecoin.
- * 
- * 
+ *
+ *
  */
-
 import {StableCoin} from "./StableCoin.sol";
 import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-contract DSCEngine is ReentrancyGuard{
+contract DSCEngine is ReentrancyGuard {
     //errors
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
@@ -21,76 +21,65 @@ contract DSCEngine is ReentrancyGuard{
 
     //modifiers
     modifier moreThanZero(uint256 amount) {
-        if(amount == 0) {
+        if (amount == 0) {
             revert DSCEngine__NeedsMoreThanZero();
         }
         _;
     }
 
     //state variables
-    mapping (address token => address priceFeed) private s_priceFeeds;
-    mapping (address user => mapping(address => uint256 amount)) private s_collateralDeposited;
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address => uint256 amount)) private s_collateralDeposited;
 
-    StableCoin immutable private i_dsc; 
+    event CollaterialDeposited(address indexed user, address indexed token, uint256 indexed amountCollateral);
 
-    
+    StableCoin private immutable i_dsc;
 
     modifier isAllowedToken(address token) {
-        if(s_priceFeeds[token] == address(0)) {
-            revert DSCEngine__TokenNotSupported();  
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__TokenNotSupported();
         }
         _;
-    } 
+    }
 
     //functions
-    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses,
-    address dscAddress) {
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
         //USD Price Feeds
-        if(tokenAddresses.length != priceFeedAddresses.length) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
             revert DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
         }
         //loop through the price feeds and add them to the mapping
-        for(uint256 i = 0; i < tokenAddresses.length; i++) {
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
         }
 
         i_dsc = StableCoin(dscAddress);
     }
 
-    function depositCollateralAndMintDSC() external{
-        
-    }
+    function depositCollateralAndMintDSC() external {}
 
     /**
-     * 
+     * @notice follows CEI: checks, effects, interactions
      * @param tokenCollateralAddress The address fo the token to be deposited as collateral
      * @param amountCollateral The amount of the token to be deposited as collateral
      */
-
-    function depositCollateral(
-        address tokenCollateralAddress, 
-        uint256 amountCollateral) 
-        moreThanZero(amountCollateral) 
-        isAllowedToken( tokenCollateralAddress)
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
         nonReentrant
-        external {
-        
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollaterialDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
     }
 
-    function redeemCollateralForDsc() external{
-    }
+    function redeemCollateralForDsc() external {}
 
-    function redeemCollateral() external{
-    }
+    function redeemCollateral() external {}
 
-    function mintDsc() external{
-    }   
+    function mintDsc() external {}
 
-    function burnDsc() external{
-    }
-    function liquidate() external{
-    }
-    function getHealthFactor() external{
-    }
-
+    function burnDsc() external {}
+    function liquidate() external {}
+    function getHealthFactor() external {}
 }
--- src/StableCoin.sol
+++ src/StableCoin.sol
@@ -6,48 +6,42 @@ import {ERC20Burnable, ERC20} from "lib/openzeppelin-contracts/contracts/token/E
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /**
- * @title StableCoin 
+ * @title StableCoin
  * @author Bowtiedgojo
  * @notice This contract is a stable coin contract that will be used to create a stable coin
  * Collataial: Exogenous
  * Minting: Algorithmic
  * Relative Stability: Pegged to USD
- * 
- * This is the contract meant to be governed by DSC engine. This contract is just the ERC20 
- * implementation of the stablecoin system. 
+ *
+ * This is the contract meant to be governed by DSC engine. This contract is just the ERC20
+ * implementation of the stablecoin system.
  */
-contract StableCoin  is ERC20Burnable, Ownable{
-
+contract StableCoin is ERC20Burnable, Ownable {
     error StableCoin__MustBeGreaterThanZero();
     error StableCoin__BurnAmountExceedsBalance();
     error StableCoin__NotZeroAddress();
 
-     
-    
-  
-    constructor() ERC20("Stablecoin", "STBL")Ownable(msg.sender){
-    }
+    constructor() ERC20("Stablecoin", "STBL") Ownable(msg.sender) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
-        if(_amount <= 0) {
+        if (_amount <= 0) {
             revert StableCoin__MustBeGreaterThanZero();
         }
-        if(balance < _amount) {
+        if (balance < _amount) {
             revert StableCoin__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner returns(bool) {
-        if(_to == address(0)) {
+    function mint(address _to, uint256 _amount) public onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert StableCoin__NotZeroAddress();
         }
-        if(_amount <= 0) {
+        if (_amount <= 0) {
             revert StableCoin__MustBeGreaterThanZero();
         }
         _mint(_to, _amount);
         return true;
     }
-
 }
