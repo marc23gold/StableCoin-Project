8c912dba4fbebe7a94a3830c49e6fd66540037c9
Marcx <haitianhero6@gmail.com>
2024-08-06 14:21:16 -0400

    mint function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -18,6 +18,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
     error DSCEngine__TokenNotSupported();
+    error DSCEngine__TransferFailed();
 
     //modifiers
     modifier moreThanZero(uint256 amount) {
@@ -30,10 +31,13 @@ contract DSCEngine is ReentrancyGuard {
     //state variables
     mapping(address token => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address => uint256 amount)) private s_collateralDeposited;
+    mapping(address user => uint256 amountDscMinted) private s_dscMinted;
 
     event CollaterialDeposited(address indexed user, address indexed token, uint256 indexed amountCollateral);
 
     StableCoin private immutable i_dsc;
+
+    address[] private s_collateralTokens;
 
     modifier isAllowedToken(address token) {
         if (s_priceFeeds[token] == address(0)) {
@@ -71,13 +75,63 @@ contract DSCEngine is ReentrancyGuard {
     {
         s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
         emit CollaterialDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
     }
 
     function redeemCollateralForDsc() external {}
 
     function redeemCollateral() external {}
 
-    function mintDsc() external {}
+    // Checks if the collateral value > DSC amount. Price feeds, value
+    /**
+     * 
+     * @param amountDscMint The amount of DSC to mint
+     * 
+     */
+    function mintDsc(uint256 amountDscMint) 
+        external
+        moreThanZero(amountDscMint)
+        nonReentrant
+        {
+            s_dscMinted[msg.sender] += amountDscMint;
+            //check: if they minted too much
+            _revertIfHealthFactorIsBroken(msg.sender);
+    }
+
+    function getAccountCollateralValue(address user) public view returns (uint256) {
+        //loop through each collateral token, get the amount they have deposited, and map it to
+        //the price, to get the USD value of the collateral 
+    }
+
+    function _getAccountInformation(address user) private view returns(uint256 totalDscMinted, uint256 collaterialValueInUsd) {
+        //total collateral deposited
+        //total DSC minted
+        //total collateral value in USD
+        totalDscMinted = s_dscMinted[user];
+        collateralvalueInUsd = getAccountCollateralValue(user);
+    }
+
+    /**
+     * 
+     * @param user The address of the user to check the health factor of
+     * @return The health factor of the user
+     * if the user goes below 1 then they can get liquidated    
+     */
+
+    function _healthFactor(address user) private view returns(uint256) {
+      //total DSC minted
+
+      //total collateral deposited 
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+    }
+
+    function _revertIfHealthFactorIsBroken(address user) internal view{
+        //Check collateral value factor
+        //revert if they don't have a healthy value factor
+    }
 
     function burnDsc() external {}
     function liquidate() external {}
