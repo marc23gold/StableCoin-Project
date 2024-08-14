95db12f57ead387cd3e2e703c91e7f88bded0c5a
Marcx <haitianhero6@gmail.com>
2024-08-14 11:48:00 -0400

    additional functionality to mint dsc function

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -25,7 +25,15 @@ contract Handler is Test {
     }
 
     function mintDsc(uint256 amount) public {
-        amount = bound(amount, 1, MAX_DEPOSIT);
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(msg.sender);
+        int256 maxDscToMint = (int256(collateralValueInUsd) / 2) - int256(totalDscMinted);
+        if (maxDscToMint < 0) {
+            return;
+        }
+        amount = bound(amount, 0, uint256(maxDscToMint));
+        if (amount == 0) {
+            return;
+        }
         vm.startPrank(msg.sender);
         dsce.mintDsc(amount);
         vm.stopPrank();
